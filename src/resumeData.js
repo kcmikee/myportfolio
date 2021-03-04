8abe7a227d818e9996fda23a0147d13c2f70be2b
kcmikee <michael.esenwa@gmail.com>
2021-03-04 10:08:18 +0100

    init

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+  imagebaseurl: "https://esenwa.github.io/",
+  name: "Kachukwu Michael Esenwa",
+  role: "Mobile and Web Developer",
+  linkedinId: "michael.esenwa@gmail.com",
+  skypeid: "+2347081293274",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/kachukwu-esenwa-a3b2a5168/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/kcmikee",
+      className: "fa fa-github",
+    },
+    {
+      name: "Twitter",
+      url: "http://twitter.com/kc_mikee",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently undergoing my national youth service in Lagos and working at Zedna technology limited. I am a self taught Full Stack Web Developer, currently diving into integration of Artificial Reality in mobile development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "Lagos, Nigeria",
+  website: "https://esenwa.github.io",
+  education: [
+    {
+      UniversityName: "Federal University Of Petroleum Resources Effurun",
+      specialization: "Bachelor of Science (Physics)",
+      MonthOfPassing: "Nov",
+      YearOfPassing: "2019",
+      // "Achievements":"Some Achiev"
+    },
+    {
+      UniversityName: "Federal Government College, Ph",
+      specialization: "SSCE",
+      MonthOfPassing: "Jun",
+      YearOfPassing: "2015",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Zedna Technology Limited",
+      specialization: "Mobile Developer",
+      MonthOfLeaving: "Present",
+      YearOfLeaving: "",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Braintemple software",
+      specialization: "Fullstack developer (Intern)",
+      MonthOfLeaving: "dec",
+      YearOfLeaving: "2018",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "WEB-DEVELOPMENT",
+    },
+    {
+      skillname: "MOBILE-DEVELOPMENT",
+    },
+    {
+      skillname: "BACKEND",
+    },
+    // {
+    //   skillname: "ReactNative",
+    // },
+    // {
+    //   skillname: "NodeJs",
+    // },
+    // {
+    //   skillname: "mySql",
+    // },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "Automobile E-commerce site",
+      imgurl: "images/portfolio/img1.png",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/img4.png",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/img3.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/img2.png",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Esenwa Henry",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
